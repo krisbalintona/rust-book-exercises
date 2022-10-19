1ec9a1af4ea1de1d64250c31b4caa6f95ce1a78d
Kristoffer Balintona <krisbalintona@gmail.com>
2022-10-19 04:58:00 -0400

    Fix indentation

--- week1/starter/src/p1_collatz.rs
+++ week1/starter/src/p1_collatz.rs
@@ -12,40 +12,40 @@
 ///
 /// Run `cargo test collatz_recursive_test` to check your answer.
 pub fn collatz_recursive(n: usize) -> usize {
-  unimplemented!()
+    unimplemented!()
 }
 
 /// Problem 1b: write an **ITERATIVE** function that computes the value of i for a given n.
 ///
 /// Run `cargo test collatz_iterative_test` to check your answer.
 pub fn collatz_iterative(n: usize) -> usize {
-  unimplemented!()
+    unimplemented!()
 }
 
 #[cfg(test)]
 mod test {
-  use super::*;
-  const COLLATZ_ANSWERS: [usize; 10] = [0, 1, 7, 2, 5, 8, 16, 3, 19, 6];
+    use super::*;
+    const COLLATZ_ANSWERS: [usize; 10] = [0, 1, 7, 2, 5, 8, 16, 3, 19, 6];
 
-  #[test]
-  fn collatz_recursive_test() {
-    for (n, answer) in COLLATZ_ANSWERS
-      .into_iter()
-      .enumerate()
-      .map(|(i, a)| (i + 1, a))
-    {
-      assert_eq!(collatz_recursive(n), answer, "n = {}", n);
+    #[test]
+    fn collatz_recursive_test() {
+        for (n, answer) in COLLATZ_ANSWERS
+            .into_iter()
+            .enumerate()
+            .map(|(i, a)| (i + 1, a))
+        {
+            assert_eq!(collatz_recursive(n), answer, "n = {}", n);
+        }
     }
-  }
 
-  #[test]
-  fn collatz_iterative_test() {
-    for (n, answer) in COLLATZ_ANSWERS
-      .into_iter()
-      .enumerate()
-      .map(|(i, a)| (i + 1, a))
-    {
-      assert_eq!(collatz_iterative(n), answer, "n = {}", n);
+    #[test]
+    fn collatz_iterative_test() {
+        for (n, answer) in COLLATZ_ANSWERS
+            .into_iter()
+            .enumerate()
+            .map(|(i, a)| (i + 1, a))
+        {
+            assert_eq!(collatz_iterative(n), answer, "n = {}", n);
+        }
     }
-  }
 }
